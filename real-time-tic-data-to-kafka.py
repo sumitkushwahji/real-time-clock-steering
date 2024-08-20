db89f3189e60a8aeb8d40db8c1ee0d6c1bfba3c3
sumitkushwahji <sumitkushwahji@gmail.com>
2024-08-20 19:40:59 +0530

    real time update tic data

--- real-time-tic-data-to-kafka.py
+++ real-time-tic-data-to-kafka.py
@@ -1,22 +1,34 @@
 from confluent_kafka import Producer
 import serial
+from datetime import datetime
+import pytz
 
 # Set up the serial connection (adjust settings as per your TIC hardware)
-ser = serial.Serial('COM3', 9600, timeout=1)
+ser = serial.Serial("COM18", 115200, timeout=1)
 
 # Set up Kafka producer
-producer = Producer({'bootstrap.servers': 'localhost:9092'})
+producer = Producer({"bootstrap.servers": "172.16.18.190:9092"})
+
+
+# Set IST timezone
+ist = pytz.timezone("Asia/Kolkata")
+
 
 def send_to_kafka(data):
-    producer.produce('tic-data-topic', data)
+    producer.produce("tic-data-topic", data)
     producer.flush()
+
 
 def collect_tic_data():
     while True:
-        line = ser.readline().decode('utf-8').strip()
+        line = ser.readline().decode("utf-8").strip()
         if line:
-            print(f"Collected data: {line}")
-            send_to_kafka(line)
+            # Get the current time in IST and format it
+            timestamp = datetime.now(ist).strftime("%Y-%m-%d %H:%M:%S %Z")
+            data_with_timestamp = f"{timestamp}: {line}"
+            print(f"Collected data: {data_with_timestamp}")
+            send_to_kafka(data_with_timestamp)
+
 
 if __name__ == "__main__":
     collect_tic_data()
